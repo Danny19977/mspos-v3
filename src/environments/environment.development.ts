92faf9ff182677cfbb835a45963a692e996fc255
KGermando <katakugermain@gmail.com>
2025-07-09 14:55:11 +0100

    chore: Remove unnecessary whitespace in environment configuration file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'http://10.163.13.144:8000/api', 
     googleMapsApiKey: 'AIzaSyChAGA30i7uZ3Z2Dq1MURL5gtngFfCbbr8'
-    
     // apiUrl: 'http://localhost:8000/api'
 };
