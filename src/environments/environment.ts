4089a5586122e217f81660d360d5074ebc5758ab
what-markeart <what-markeart@gmail.com>
2026-05-01 00:44:44 +0100

    fix(environment): correct apiUrl assignment order in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true, 
-    apiUrl: 'https://mspos-api-dvp8.onrender.com/api',
-    // apiUrl: 'http://192.168.0.20/api',
+    // apiUrl: 'https://mspos-api-dvp8.onrender.com/api',
+    apiUrl: 'http://192.168.0.20/api',
     googleMapsApiKey: 'AIzaSyChAGA30i7uZ3Z2Dq1MURL5gtngFfCbbr8'
 };
